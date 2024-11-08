1181e94bbab01f753b13ea0200ef87e3978a2467
anikmalik30 <anik.nntsoftware@gmail.com>
2024-11-08 16:58:10 +0530

    Comment out unused language change logic in Layout component for cleanup

--- src/components/App/Layout.js
+++ src/components/App/Layout.js
@@ -2,17 +2,17 @@ import React, { useEffect } from "react"
 import AOS from "aos"
 import GoTop from "./GoTop"
 import DarkAndLightMode from "./DarkAndLightMode"
-import { useI18next } from "gatsby-plugin-react-i18next"
+// import { useI18next } from "gatsby-plugin-react-i18next"
 const Layout = ({ children }) => {
   React.useEffect(() => {
     AOS.init()
   }, [])
-  const { changeLanguage } = useI18next();
+  // const { changeLanguage } = useI18next();
 
-  useEffect(() => {
-    const userLang = sessionStorage.getItem("siteLanguage") || "en";
-    changeLanguage(userLang);
-  }, []);
+  // useEffect(() => {
+  //   const userLang = localStorage.getItem("siteLanguage") || "en";
+  //   changeLanguage(userLang);
+  // }, []);
 
   return (
     <>
