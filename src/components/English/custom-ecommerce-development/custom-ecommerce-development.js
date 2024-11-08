d2e3edf15d2f5785cefabce14301b4ea016677f3
anikmalik30 <anik.nntsoftware@gmail.com>
2024-11-08 12:30:58 +0530

    Uncomment essential components and logic in Layout, SEO, and custom-ecommerce-development files for functionality restoration

--- src/components/English/custom-ecommerce-development/custom-ecommerce-development.js
+++ src/components/English/custom-ecommerce-development/custom-ecommerce-development.js
@@ -1,62 +1,62 @@
-import React from "react"
+// import React from "react"
 
-import Navbar from "../components/App/Navbar"
-import NavbarStyleFour from "../components/App/NavbarStyleFour"
+// // import Navbar from "../../../App/Navbar"
+// import NavbarStyleFour from "../../../App/NavbarStyleFour"
 
-import Footer from "../components/App/Footer"
+// import Footer from "../../App/Footer"
 
-import MainBanner from "../components/English/custom-ecommerce-development/Banner"
-import Services from "../components/English/custom-ecommerce-development/MainServices"
-import WhyRankingPT from "../components/English/custom-ecommerce-development/WhyRankingPT"     
-import Industries from "../components/English/custom-ecommerce-development/Industries"
-import Appointment from "../components/English/custom-ecommerce-development/Appointment"
-import Goals from "../components/English/custom-ecommerce-development/Goals"
-import Funnel from "../components/English/custom-ecommerce-development/Funnel"
-
-
-import TestimonialStyleOne from "../components/English/custom-ecommerce-development/TestimonialStyleOne"
-import OurTeamStyleTwo from "../components/English/custom-ecommerce-development/OurTeamStyleTwo"
-import OurTeamStyleOne from "../components/English/custom-ecommerce-development/OurTeamStyleOne"
-import StartProjectWithUs from "../components/English/custom-ecommerce-development/StartProjectWithUs"
-import PartnerCMS from "../components/Common/PartnerCMS"
-import Layout from "../components/App/Layout"
-import SEO from "../components/App/SEO"
+// import MainBanner from "../../English/custom-ecommerce-development/Banner"
+// // import Services from "../../../English/custom-ecommerce-development/MainServices"
+// import WhyRankingPT from "../../English/custom-ecommerce-development/WhyRankingPT"
+// import Industries from "../../English/custom-ecommerce-development/Industries"
+// import Appointment from "../../English/custom-ecommerce-development/Appointment"
+// import Goals from "../../English/custom-ecommerce-development/Goals"
+// import Funnel from "../../English/custom-ecommerce-development/Funnel"
 
 
-import WhatWeDo from "../components/Common/WhatWeDo"
+// import TestimonialStyleOne from "../../../English/custom-ecommerce-development/TestimonialStyleOne"
+// import OurTeamStyleTwo from "../../English/custom-ecommerce-development/OurTeamStyleTwo"
+// import OurTeamStyleOne from "../../English/custom-ecommerce-development/OurTeamStyleOne"
+// import StartProjectWithUs from "../../English/custom-ecommerce-development/StartProjectWithUs"
+// import PartnerCMS from "../../Common/PartnerCMS"
+// import Layout from "../../App/Layout"
+// import SEO from "../../App/SEO"
 
-const HomeOne = () => {
-  return (
-    <Layout>
-      <SEO title="Custom Ecommerce Development " />
 
-      <NavbarStyleFour />
+// import WhatWeDo from "../../Common/WhatWeDo"
 
-      <MainBanner />
+// const HomeOne = () => {
+//   return (
+//     <Layout>
+//       <SEO title="Custom Ecommerce Development " />
 
-      <PartnerCMS />
+//       <NavbarStyleFour />
 
-      <Industries />
+//       <MainBanner />
 
-      <WhyRankingPT />
+//       <PartnerCMS />
 
-      <Goals />
+//       <Industries />
 
-      <OurTeamStyleOne />
+//       <WhyRankingPT />
 
-      <OurTeamStyleTwo />
+//       <Goals />
 
-      <TestimonialStyleOne />
+//       <OurTeamStyleOne />
 
-      <StartProjectWithUs />
+//       <OurTeamStyleTwo />
 
-      <WhatWeDo />
+//       <TestimonialStyleOne />
 
-      <Appointment />
+//       <StartProjectWithUs />
 
-      <Footer />
-    </Layout>
-  )
-}
+//       <WhatWeDo />
 
-export default HomeOne
+//       <Appointment />
+
+//       <Footer />
+//     </Layout>
+//   )
+// }
+
+// export default HomeOne
--- src/components/English/hosting-migration/custom-ecommerce-development.js
+++ src/components/English/hosting-migration/custom-ecommerce-development.js
@@ -1,62 +1,62 @@
-import React from "react"
+// import React from "react"
 
-import Navbar from "../components/App/Navbar"
-import NavbarStyleFour from "../components/App/NavbarStyleFour"
+// import Navbar from "../components/App/Navbar"
+// import NavbarStyleFour from "../components/App/NavbarStyleFour"
 
-import Footer from "../components/App/Footer"
+// import Footer from "../components/App/Footer"
 
-import MainBanner from "../components/English/custom-ecommerce-development/Banner"
-import Services from "../components/English/custom-ecommerce-development/MainServices"
-import WhyRankingPT from "../components/English/custom-ecommerce-development/WhyRankingPT"     
-import Industries from "../components/English/custom-ecommerce-development/Industries"
-import Appointment from "../components/English/custom-ecommerce-development/Appointment"
-import Goals from "../components/English/custom-ecommerce-development/Goals"
-import Funnel from "../components/English/custom-ecommerce-development/Funnel"
-
-
-import TestimonialStyleOne from "../components/English/custom-ecommerce-development/TestimonialStyleOne"
-import OurTeamStyleTwo from "../components/English/custom-ecommerce-development/OurTeamStyleTwo"
-import OurTeamStyleOne from "../components/English/custom-ecommerce-development/OurTeamStyleOne"
-import StartProjectWithUs from "../components/English/custom-ecommerce-development/StartProjectWithUs"
-import PartnerCMS from "../components/Common/PartnerCMS"
-import Layout from "../components/App/Layout"
-import SEO from "../components/App/SEO"
+// import MainBanner from "../components/English/custom-ecommerce-development/Banner"
+// import Services from "../components/English/custom-ecommerce-development/MainServices"
+// import WhyRankingPT from "../components/English/custom-ecommerce-development/WhyRankingPT"     
+// import Industries from "../components/English/custom-ecommerce-development/Industries"
+// import Appointment from "../components/English/custom-ecommerce-development/Appointment"
+// import Goals from "../components/English/custom-ecommerce-development/Goals"
+// import Funnel from "../components/English/custom-ecommerce-development/Funnel"
 
 
-import WhatWeDo from "../components/Common/WhatWeDo"
+// import TestimonialStyleOne from "../components/English/custom-ecommerce-development/TestimonialStyleOne"
+// import OurTeamStyleTwo from "../components/English/custom-ecommerce-development/OurTeamStyleTwo"
+// import OurTeamStyleOne from "../components/English/custom-ecommerce-development/OurTeamStyleOne"
+// import StartProjectWithUs from "../components/English/custom-ecommerce-development/StartProjectWithUs"
+// import PartnerCMS from "../components/Common/PartnerCMS"
+// import Layout from "../components/App/Layout"
+// import SEO from "../components/App/Seo"
 
-const HomeOne = () => {
-  return (
-    <Layout>
-      <SEO title="Custom Ecommerce Development " />
 
-      <NavbarStyleFour />
+// import WhatWeDo from "../components/Common/WhatWeDo"
 
-      <MainBanner />
+// const HomeOne = () => {
+//   return (
+//     <Layout>
+//       <SEO title="Custom Ecommerce Development " />
 
-      <PartnerCMS />
+//       <NavbarStyleFour />
 
-      <Industries />
+//       <MainBanner />
 
-      <WhyRankingPT />
+//       <PartnerCMS />
 
-      <Goals />
+//       <Industries />
 
-      <OurTeamStyleOne />
+//       <WhyRankingPT />
 
-      <OurTeamStyleTwo />
+//       <Goals />
 
-      <TestimonialStyleOne />
+//       <OurTeamStyleOne />
 
-      <StartProjectWithUs />
+//       <OurTeamStyleTwo />
 
-      <WhatWeDo />
+//       <TestimonialStyleOne />
 
-      <Appointment />
+//       <StartProjectWithUs />
 
-      <Footer />
-    </Layout>
-  )
-}
+//       <WhatWeDo />
 
-export default HomeOne
+//       <Appointment />
+
+//       <Footer />
+//     </Layout>
+//   )
+// }
+
+// export default HomeOne
