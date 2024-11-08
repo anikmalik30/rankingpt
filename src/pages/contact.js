e344acf1a76ccf455463e2f228ad5c6de847efe5
anikmalik30 <anik.nntsoftware@gmail.com>
2024-11-08 18:05:33 +0530

    Uncomment SvgIcon import and usage in Contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,7 +5,7 @@ import Navbar from "../components/App/Navbar"
 // import PageBanner from "../components/Common/PageBanner"
 import Footer from "../components/App/Footer"
 import ContactForm from "../components/Contact/ContactForm"
-// import SvgIcon from "../components/SvgIcon";
+import SvgIcon from "../components/SvgIcon";
 
 const Contact = () => {
   return (
@@ -15,7 +15,7 @@ const Contact = () => {
       <Navbar />
 
       <div className="contact-info-area pt-100 pb-70">
-        {/* <SvgIcon name="084-approved" height={50} width={50} />just give the name of the svg file */}
+        <SvgIcon name="084-approved" height={50} width={50} />just give the name of the svg file
       </div>
 
       <ContactForm />
