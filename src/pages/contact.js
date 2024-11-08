22d0c0eaaa98a6aa807f30a5a36fd58bb3162bc4
anikmalik30 <anik.nntsoftware@gmail.com>
2024-11-08 17:15:52 +0530

    Uncomment Layout, SEO, Navbar, and Footer imports in contact page for proper rendering

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,29 +1,29 @@
 import React, { useEffect } from "react";
-// import Layout from "../components/App/Layout"
-// import SEO from "../components/App/SEO"
-// import Navbar from "../components/App/Navbar"
-// // import PageBanner from "../components/Common/PageBanner"
-// import Footer from "../components/App/Footer"
+import Layout from "../components/App/Layout"
+import SEO from "../components/App/SEO"
+import Navbar from "../components/App/Navbar"
+// import PageBanner from "../components/Common/PageBanner"
+import Footer from "../components/App/Footer"
 // import ContactForm from "../components/Contact/ContactForm"
 // import SvgIcon from "../components/SvgIcon";
 
 // import PageSpeedIcon from "../assets/icons/pagespeed.svg";
 const Contact = () => {
   return (
-    // <Layout>
-    //   <SEO title="Contact" />
+    <Layout>
+      <SEO title="Contact" />
 
-    //   <Navbar />
+      <Navbar />
 
-    //   <div className="contact-info-area pt-100 pb-70">
-    //     <SvgIcon name="084-approved" height={50} width={50} />{/* just give the name of the svg file */}
-    //   </div>
+      <div className="contact-info-area pt-100 pb-70">
+        <SvgIcon name="084-approved" height={50} width={50} />{/* just give the name of the svg file */}
+      </div>
 
-    //   <ContactForm />
+      {/* <ContactForm /> */}
 
-    //   <Footer />
-    // </Layout>
-    <p>contact</p>
+      <p>contact</p>
+      <Footer />
+    </Layout>
   )
 }
 
