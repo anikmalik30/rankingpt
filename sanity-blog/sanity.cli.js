b21fdf32e307213e00caa23d56fe9c0d61bd4ff7
anikmalik30 <anik.nntsoftware@gmail.com>
2024-11-08 15:15:19 +0530

    Update projectId in sanity.cli.js and restore SEO component functionality in SEO.js

--- sanity-blog/sanity.cli.js
+++ sanity-blog/sanity.cli.js
@@ -2,7 +2,7 @@ import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 's9yprksh',
+    projectId: 'hvgr1w7n',
     dataset: 'production'
   },
   /**
--- src/components/App/SEO.js
+++ src/components/App/SEO.js
@@ -1,9 +1,9 @@
-/**
- * SEO component that queries for data with
- *  Gatsby's useStaticQuery React hook
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
+// /**
+//  * SEO component that queries for data with
+//  *  Gatsby's useStaticQuery React hook
+//  *
+//  * See: https://www.gatsbyjs.com/docs/use-static-query/
+//  */
 
 // import React from "react"
 // import PropTypes from "prop-types"
@@ -11,8 +11,8 @@
 // import { useStaticQuery, graphql } from "gatsby"
 
 // function SEO({ description, lang, meta, title }) {
-//   const { site } = useStaticQuery(
-//     graphql`
+//     const { site } = useStaticQuery(
+//         graphql`
 //       query {
 //         site {
 //           siteMetadata {
@@ -23,67 +23,137 @@
 //         }
 //       }
 //     `
-//   )
+//     )
 
-//   const metaDescription = description || site.siteMetadata.description
-//   const defaultTitle = site.siteMetadata?.title
+//     const metaDescription = description || site.siteMetadata.description
+//     const defaultTitle = site.siteMetadata?.title
 
-//   return (
-//     <Helmet
-//       htmlAttributes={{
-//         lang,
-//       }}
-//       title={title}
-//       titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
-//       meta={[
-//         {
-//           name: `description`,
-//           content: metaDescription,
-//         },
-//         {
-//           property: `og:title`,
-//           content: title,
-//         },
-//         {
-//           property: `og:description`,
-//           content: metaDescription,
-//         },
-//         {
-//           property: `og:type`,
-//           content: `website`,
-//         },
-//         {
-//           name: `twitter:card`,
-//           content: `summary`,
-//         },
-//         {
-//           name: `twitter:creator`,
-//           content: site.siteMetadata?.author || ``,
-//         },
-//         {
-//           name: `twitter:title`,
-//           content: title,
-//         },
-//         {
-//           name: `twitter:description`,
-//           content: metaDescription,
-//         },
-//       ].concat(meta)}
-//     />
-//   )
+//     return (
+//         <Helmet
+//             htmlAttributes={{
+//                 lang,
+//             }}
+//             title={title}
+//             titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
+//             meta={[
+//                 {
+//                     name: `description`,
+//                     content: metaDescription,
+//                 },
+//                 {
+//                     property: `og:title`,
+//                     content: title,
+//                 },
+//                 {
+//                     property: `og:description`,
+//                     content: metaDescription,
+//                 },
+//                 {
+//                     property: `og:type`,
+//                     content: `website`,
+//                 },
+//                 {
+//                     name: `twitter:card`,
+//                     content: `summary`,
+//                 },
+//                 {
+//                     name: `twitter:creator`,
+//                     content: site.siteMetadata?.author || ``,
+//                 },
+//                 {
+//                     name: `twitter:title`,
+//                     content: title,
+//                 },
+//                 {
+//                     name: `twitter:description`,
+//                     content: metaDescription,
+//                 },
+//             ].concat(meta)}
+//         />
+//     )
 // }
 
 // SEO.defaultProps = {
-//   lang: `en`,
-//   meta: [],
-//   description: ``,
+//     lang: `en`,
+//     meta: [],
+//     description: ``,
 // }
 
 // SEO.propTypes = {
-//   description: PropTypes.string,
-//   lang: PropTypes.string,
-//   meta: PropTypes.arrayOf(PropTypes.object),
-//   title: PropTypes.string.isRequired,
+//     description: PropTypes.string,
+//     lang: PropTypes.string,
+//     meta: PropTypes.arrayOf(PropTypes.object),
+//     title: PropTypes.string.isRequired,
 // }
 
 // export default SEO
+
+import React from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
+
+function SEO({ description, lang, meta, title, author }) {
+    const metaDescription = description || "";
+    const defaultTitle = title || "";
+
+    return (
+        <Helmet
+            htmlAttributes={{
+                lang,
+            }}
+            title={title}
+            titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
+            meta={[
+                {
+                    name: `description`,
+                    content: metaDescription,
+                },
+                {
+                    property: `og:title`,
+                    content: title,
+                },
+                {
+                    property: `og:description`,
+                    content: metaDescription,
+                },
+                {
+                    property: `og:type`,
+                    content: `website`,
+                },
+                {
+                    name: `twitter:card`,
+                    content: `summary`,
+                },
+                {
+                    name: `twitter:creator`,
+                    content: author || ``,
+                },
+                {
+                    name: `twitter:title`,
+                    content: title,
+                },
+                {
+                    name: `twitter:description`,
+                    content: metaDescription,
+                },
+            ].concat(meta)}
+        />
+    );
+}
+
+SEO.defaultProps = {
+    lang: `en`,
+    meta: [],
+    description: ``,
+    author: ``,
+};
+
+SEO.propTypes = {
+    description: PropTypes.string,
+    lang: PropTypes.string,
+    meta: PropTypes.arrayOf(PropTypes.object),
+    title: PropTypes.string.isRequired,
+    author: PropTypes.string,
+};
+
+export default SEO;
