c6d59d6c08535a80ae10d168be5093ed17a8342c
anikmalik30 <anik.nntsoftware@gmail.com>
2024-11-08 17:52:05 +0530

    Refactor: Move archived pages to src/pages and update import paths

---
+++ src/pages/instagram-ads.js
@@ -0,0 +1,43 @@
+import React from "react"
+
+import Navbar from "../components/App/Navbar"
+import Footer from "../components/App/Footer"
+import MainBanner from "../components/English/InstagramAds/MainBanner"
+
+import FAQInstagramAds from "../components/English/InstagramAds/FAQInstagramAds"
+import InstagramAds2 from "../components/English/InstagramAds/InstagramAds2"
+import InstagramAds3 from "../components/English/InstagramAds/InstagramAds3"
+import InstagramAds4 from "../components/English/InstagramAds/InstagramAds4"
+import InstagramServices from "../components/English/InstagramAds/InstagramServices"
+import Layout from "../components/App/Layout"
+import SEO from "../components/App/SEO"
+
+
+import WhatWeDo from "../components/Common/WhatWeDo"
+
+const InstagramAds = () => {
+  return (
+    <Layout>
+      <SEO title="Ecommerce SEO " />
+
+      <Navbar />
+
+      <MainBanner />
+
+      <InstagramServices />
+
+      <InstagramAds2 />
+
+      <InstagramAds4 />
+
+      <InstagramAds3 />
+
+      <FAQInstagramAds />
+
+
+      <Footer />
+    </Layout>
+  )
+}
+
+export default InstagramAds
